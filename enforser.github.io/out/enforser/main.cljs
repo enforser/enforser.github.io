df0d6830869c00fe554686d1fa219dae8617507a
enforser <matthew.fors@wework.com>
2019-12-04 10:27:16 -0500

    using clojurescript

---
+++ enforser.github.io/out/enforser/main.cljs
@@ -0,0 +1,4 @@
+(ns enforser.main
+  (:require [reagent.core :as r]))
+
+(js/console.log "This is my log")
---
+++ enforser.github.io/out/enforser/main.js
@@ -0,0 +1,7 @@
+// Compiled by ClojureScript 1.10.597 {}
+goog.provide('enforser.main');
+goog.require('cljs.core');
+goog.require('reagent.core');
+console.log("This is my log");
+
+//# sourceMappingURL=main.js.map
