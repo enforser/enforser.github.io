0742e774e94b6bd768be5038309383c8afbe7cbe
enforser <matt@spacious.com>
2020-03-16 22:56:55 -0400

    all different now

---
+++ blog/src/blog/pages/contact.clj
@@ -0,0 +1,16 @@
+(ns blog.pages.contact)
+
+(defn page
+  [context]
+  [:div {:class "container"}
+   [:div {:class "row"}
+    [:pre
+     [:code
+     "
+#
+# How to get in touch:
+#
+     "
+     [:li [:a {:href "mailto:trailcapital@gmail.com"} "E-mail"]]
+     [:li [:a {:href "https://www.linkedin.com/in/matthew-fors-8b655699/"} "LinkedIn"]]
+     [:li [:a {:href "https://www.twitter.com/fattmors"} "Twitter"]]]]]])
