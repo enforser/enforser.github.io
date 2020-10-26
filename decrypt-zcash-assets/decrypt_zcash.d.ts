e7178afd974fc82e71b7aba47b0bfc1073a71166
enforser <matthew.fors@gemini.com>
2020-10-26 11:15:08 -0400

    decrypt-zcash-assets

---
+++ decrypt-zcash-assets/decrypt_zcash.d.ts
@@ -0,0 +1,28 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} raw_tx
+* @param {string} ock
+* @returns {any}
+*/
+export function greet(raw_tx: string, ock: string): any;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly greet: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
+        
